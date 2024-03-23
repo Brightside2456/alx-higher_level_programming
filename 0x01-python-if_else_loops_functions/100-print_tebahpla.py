a6bbfd2c2d8545c8a7883c72ba13bc2d103ea775
Bright Sefah <sefahb13@gmail.com>
2024-03-23 12:37:04 +0000

    Alternate between capital and small case aplphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-letter = 122
-for i in range(1, 26):
-    print(chr(letter), end="")
-    if letter > 97:
-        letter = letter - 32 - i
-    else:
-        letter = letter + 32 - i
+for i in range(122, 96, -1):
+  if i % 2 == 0:
+    print(chr(i), end="")
+  else:
+    print(chr(i - 32), end="")
+
