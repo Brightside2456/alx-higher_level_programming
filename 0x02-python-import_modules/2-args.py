112f770d0e949104e0a6fc978dce3af6c51a3759
Bright Sefah <sefahb13@gmail.com>
2024-08-17 01:56:45 +0000

    Argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv)
-    arg_str  = "argument" if length - 1 == 1 else "arguments"
+    arg_str  = "argument:" if length - 1 == 1 else "arguments:"
     print("{} {}".format(length - 1, arg_str))
     for i in range(1, length, 1):
         print("{} : {}".format(i, sys.argv[i]))
